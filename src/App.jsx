81343b9e92831b9444747faf42dc89c573f47338
abdul-hakeem01 <166890202+abdul-hakeem01@users.noreply.github.com>
2024-10-06 17:55:37 +0530

    Update and rename App.js to App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './index.css';
+import TaskCard from './TaskCard';
+
+const App = () => {
+  return (
+    <div className="App">
+      <TaskCard />
+    </div>
+  );
+};
+
+export default App;
