23bcff47c383059f04a6c2354f343ef012c3878e
abdul-hakeem01 <166890202+abdul-hakeem01@users.noreply.github.com>
2024-10-06 17:56:29 +0530

    Create TaskCard.jsx

---
+++ src/TaskCard.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './TaskCard.css';
+
+const TaskCard = () => {
+  return (
+    <div className="task-card">
+      <h2>Task Title</h2>
+      <p>Task Description</p>
+    </div>
+  );
+};
+
+export default TaskCard;
